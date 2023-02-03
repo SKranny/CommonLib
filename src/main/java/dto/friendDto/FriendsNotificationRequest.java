d3cebcf286ba4bc3b5508b81110c4f007c4226f5
nikod <nikodim.curo4kin@yandex.ru>
2023-02-03 16:51:39 +0300

    Friends reqNotfDto update.

--- src/main/java/dto/friendDto/FriendsNotificationRequest.java
+++ src/main/java/dto/friendDto/FriendsNotificationRequest.java
@@ -1,5 +1,6 @@
 package dto.friendDto;
 
+import dto.notification.ContentDTO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,5 +15,7 @@ public class FriendsNotificationRequest {
     private Long srcPersonId;
 
     private Long dstPersonId;
+
+    private ContentDTO content;
 
 }
