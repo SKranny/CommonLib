7adbd246eec317efcf1f691907b36742c2dee229
Дмитрий Кечин <kechin.d@gmail.com>
2023-02-06 18:10:31 +0300

    add shortPersonDto

--- src/main/java/dto/userDto/ShortPersonDTO.java
+++ src/main/java/dto/userDto/ShortPersonDTO.java
@@ -1,10 +1,18 @@
 package dto.userDto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ShortPersonDTO {
     private Long id;
 
