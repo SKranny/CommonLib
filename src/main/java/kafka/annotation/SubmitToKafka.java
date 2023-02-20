c8f576f6dc2ff40b5a5026869832d007d6a9e179
Дмитрий Кечин <kechin.d@gmail.com>
2023-02-21 01:11:27 +0300

    add security

--- src/main/java/kafka/annotation/SubmitToKafka.java
+++ src/main/java/kafka/annotation/SubmitToKafka.java
@@ -3,7 +3,7 @@ package kafka.annotation;
 import java.lang.annotation.*;
 
 @Inherited
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SubmitToKafka {
     String[] topic();
